dd6a6208de9aa540d178cc6cd20f0f225997f06c
DjoninhaA <djonathan8016@gmail.com>
2023-11-23 20:44:20 -0300

    Retirando a aba de inserção dos resultados, foi criada porem não temos dados para inserir

--- schemas/index.js
+++ schemas/index.js
@@ -5,9 +5,8 @@ import diretrizes from './diretrizes'
 import objetivo from './objetivo'
 import pontosPositivos from './pontosPositivos'
 import membros from './membros'
-import resultados from './resultados'
 import faq from './faq'
 import pilares from './pilares'
 import estatuto from './estatuto'
 
-export const schemaTypes = [pessoa, qrcode, diretrizes, objetivo, pontosPositivos, membros, resultados, faq, estatuto, pilares ]
+export const schemaTypes = [pessoa, qrcode, diretrizes, objetivo, pontosPositivos, membros, faq, estatuto, pilares ]
